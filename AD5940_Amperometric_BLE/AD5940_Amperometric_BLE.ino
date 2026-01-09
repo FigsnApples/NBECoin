9971d33aa1c876fa685ddd498cd1dae43fb63659
Jose Castillo <bjoe.cv@gmail.com>
2026-01-09 11:57:58 -0800

    Implement AD5940 Amperometric BLE functionality
    This file implements the main functionality for the AD5940 amperometric BLE application, including initialization, configuration, and data handling.
    
    -copilot lol

---
+++ AD5940_Amperometric_BLE/AD5940_Amperometric_BLE.ino
@@ -0,0 +1,160 @@
+#include "ad5940.h"
+#include "Amperometric.h"
+#include "BleComm.h"
+
+#include <LibPrintf.h>  //allows to use c-style print() statement instead of Arduino style Serial.print()
+#include "BleComm.h"  // BLE streaming
+
+
+#define APPBUFF_SIZE 1000
+
+#ifdef __ICCARM__
+#pragma location = "never_retained_ram"
+#endif
+
+uint32_t AppBuff[APPBUFF_SIZE];
+float LFOSCFreq;
+/* It's your choice here how to do with the data. Here is just an example to print them to UART */
+int32_t AMPShowResult(float *pData, uint32_t DataCount)
+{
+  /* Print data*/
+  for(int i=0;i<DataCount;i++)
+  {
+    //String dat = String(pData[i]);
+    //sendMessage(dat);
+    printf("Current:%f\n", pData[i]);
+      ble_push_current(pData[i]);  //change here from OG code -jose //incorporates bluetooth 
+}
+  return 0;
+}
+
+/* Initialize AD5940 basic blocks like clock */
+static int32_t AD5940PlatformCfg(void)
+{
+  CLKCfg_Type clk_cfg;
+  FIFOCfg_Type fifo_cfg;
+  SEQCfg_Type seq_cfg;
+  AGPIOCfg_Type gpio_cfg;
+  LFOSCMeasure_Type LfoscMeasure;
+  /* Use hardware reset */
+  AD5940_HWReset();
+  /* Platform configuration */
+  AD5940_Initialize();
+  /* Step1. Configure clock */
+  clk_cfg.HFOSCEn = bTRUE;
+  clk_cfg.HFXTALEn = bFALSE;
+  clk_cfg.LFOSCEn = bTRUE;
+  clk_cfg.HfOSC32MHzMode = bFALSE;
+  clk_cfg.SysClkSrc = SYSCLKSRC_HFOSC;
+  clk_cfg.SysClkDiv = SYSCLKDIV_1;
+  clk_cfg.ADCCLkSrc = ADCCLKSRC_HFOSC;
+  clk_cfg.ADCClkDiv = ADCCLKDIV_1;
+  AD5940_CLKCfg(&clk_cfg);
+  /* Step2. Configure FIFO and Sequencer*/
+  fifo_cfg.FIFOEn = bFALSE;
+  fifo_cfg.FIFOMode = FIFOMODE_FIFO;
+  fifo_cfg.FIFOSize = FIFOSIZE_4KB;                      /* 4kB for FIFO, The reset 2kB for sequencer */
+  fifo_cfg.FIFOSrc = FIFOSRC_DFT; //SINC2NOTCH; //changes made here used to be fifosrc_dft -jose 
+  fifo_cfg.FIFOThresh = 1;    //changes made here   
+  AD5940_FIFOCfg(&fifo_cfg);                             /* Disable to reset FIFO. */
+  fifo_cfg.FIFOEn = bTRUE;  
+  AD5940_FIFOCfg(&fifo_cfg);                             /* Enable FIFO here */
+  /* Configure sequencer and stop it */
+  seq_cfg.SeqMemSize = SEQMEMSIZE_2KB;
+  seq_cfg.SeqBreakEn = bFALSE;
+  seq_cfg.SeqIgnoreEn = bFALSE;
+  seq_cfg.SeqCntCRCClr = bTRUE;
+  seq_cfg.SeqEnable = bFALSE;
+  seq_cfg.SeqWrTimer = 0;
+  AD5940_SEQCfg(&seq_cfg);
+  
+  /* Step3. Interrupt controller */
+  AD5940_INTCCfg(AFEINTC_1, AFEINTSRC_ALLINT, bTRUE);           /* Enable all interrupt in Interrupt Controller 1, so we can check INTC flags */
+  AD5940_INTCCfg(AFEINTC_0, AFEINTSRC_DATAFIFOTHRESH, bTRUE);   /* Interrupt Controller 0 will control GP0 to generate interrupt to MCU */
+  AD5940_INTCClrFlag(AFEINTSRC_ALLINT);
+  /* Step4: Reconfigure GPIO */
+  gpio_cfg.FuncSet = GP6_SYNC|GP5_SYNC|GP4_SYNC|GP2_SYNC|GP1_SLEEP|GP0_INT;
+  gpio_cfg.InputEnSet = AGPIO_Pin2;
+  gpio_cfg.OutputEnSet = AGPIO_Pin0|AGPIO_Pin1|AGPIO_Pin4|AGPIO_Pin5|AGPIO_Pin6;
+  gpio_cfg.OutVal = AGPIO_Pin1; //set high to turn off LED
+  gpio_cfg.PullEnSet = 0;
+  AD5940_AGPIOCfg(&gpio_cfg);
+  
+  AD5940_SleepKeyCtrlS(SLPKEY_UNLOCK);  /* Allow AFE to enter sleep mode. */
+  /* Measure LFOSC frequency */
+  LfoscMeasure.CalDuration = 1000.0;  /* 1000ms used for calibration. */
+  LfoscMeasure.CalSeqAddr = 0;
+  LfoscMeasure.SystemClkFreq = 16000000.0f; /* 16MHz in this firmware. */
+  AD5940_LFOSCMeasure(&LfoscMeasure, &LFOSCFreq);
+  //printf("Measured LFOSC Freq (used for sequencer timing) - rounded: %d Hz\n", (int)(LFOSCFreq + 0.5)); 
+  return 0;
+}
+
+/* !!Change the application parameters here if you want to change it to none-default value */
+void AD5940AMPStructInit(void)
+{
+  AppAMPCfg_Type *pAMPCfg;
+  
+  AppAMPGetCfg(&pAMPCfg);
+  pAMPCfg->WuptClkFreq = LFOSCFreq;
+  /* Configure general parameters */
+  pAMPCfg->SeqStartAddr = 0;
+  pAMPCfg->MaxSeqLen = 512;     /* @todo add checker in function */  
+  pAMPCfg->RcalVal = 10000.0;
+  pAMPCfg->NumOfData = -1;      /* Never stop until you stop it manually by //changes made here if -1 it will never stop outputting data used to be 25 AppAMPCtrl() function */  
+  
+  /*TOTAL MEASUREMENTS = NunOfData * FifoThresh
+  /* Configure measurement parameters */
+  pAMPCfg->AmpODR = 1.5;            /* Time between samples in seconds */
+  pAMPCfg->FifoThresh = 1;          /* Number of measurements before alerting host microcontroller */
+  
+  pAMPCfg->SensorBias = -300;        /* Sensor bias voltage between reference and sense electrodes*/
+  pAMPCfg->LptiaRtiaSel = LPTIARTIA_256K;
+  pAMPCfg->LpTiaRl = LPTIARLOAD_3K1;
+  pAMPCfg->Vzero = 1100;            /* Vzero voltage. Voltage on Sense electrode. Unit is mV*/
+  
+  pAMPCfg->ADCRefVolt = 1.82;   /* Measure voltage on Vref_1V8 pin */
+}
+
+void AD5940_Main(void)
+{
+  uint32_t temp;
+  
+  AD5940PlatformCfg();
+  AD5940AMPStructInit(); /* Configure your parameters in this function */ 
+  AppAMPInit(AppBuff, APPBUFF_SIZE);    /* Initialize AMP application. Provide a buffer, which is used to store sequencer commands */
+  AppAMPCtrl(AMPCTRL_START, 0);         /* Control AMP measurement to start. Second parameter has no meaning with this command. */
+  
+  while(1)
+  {
+    /* Check if interrupt flag which will be set when interrupt occurred. */
+    if(AD5940_GetMCUIntFlag())
+    {
+      //digitalWrite(13, HIGH);
+      AD5940_ClrMCUIntFlag(); /* Clear this flag */
+      temp = APPBUFF_SIZE;
+      AppAMPISR(AppBuff, &temp); /* Deal with it and provide a buffer to store data we got */
+      AMPShowResult((float*)AppBuff, temp); /* Show the results to UART */
+    }
+    //digitalWrite(13, LOW);
+  }
+}
+
+void setup() {
+  ble_init();  // start BLE advertising for amperometric data
+
+  //Serial.begin(115200);
+  ////pinMode(13, OUTPUT);
+
+  printf("start\n");
+  //digitalWrite(13, HIGH);
+  AD5940_MCUResourceInit(NULL);
+  printf("initialized\n");
+  AD5940_Main();
+  //digitalWrite(13, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
