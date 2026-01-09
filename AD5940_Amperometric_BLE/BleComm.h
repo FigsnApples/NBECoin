4edce71d19fab1c68697a2c2a39ee070e30d5818
Jose Castillo <bjoe.cv@gmail.com>
2026-01-09 12:06:03 -0800

    Update BleComm.h

--- AD5940_Amperometric_BLE/BleComm.h
+++ AD5940_Amperometric_BLE/BleComm.h
@@ -1,1 +1,20 @@
+#pragma once
+// BleComm.h - Minimal Bluedroid BLE interface for amperometric streaming
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Initialize BLE (advertises a UART-like service that notifies text samples)
+void ble_init(void);
+
+// Push one current value (in microamps) as text notification: "current=<value>"
+void ble_push_current(float amps_uA);
+
+// Push an array of current values (sends multiple notifications, simple loop)
+void ble_push_buffer(const float* vals, unsigned int count);
+
+#ifdef __cplusplus
+}
+#endif
 
